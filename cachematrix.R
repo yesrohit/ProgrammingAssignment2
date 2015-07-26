5df2703832efd0fa1251f9c8e73247a665c5967f
yesrohit <yesrohit@gmail.com>
2015-07-26 17:05:56 -0500

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,10 @@
 ## Two functions have been written: 'makeCacheMatrix' which takes a matrix as an input and the 'cacheSolve' function
 ## which takes an instance of the makeCacheMatrix function to see if it already has an inverse calculated. If it does, 
-## it reports the value of the inverse. 
+## it reports the value of the stored inverse otherwise calculates it and reports the inverse. 
 
 ## Write a short comment describing this function
 ## makeCacheMatrix: This is the function called when initializing a matrix. The matrix may be initialized as follows:
-## Mat1<-makeCacheMatrix(matrix(rnorm(9),3,3))
+## Eg. Mat1<-makeCacheMatrix(matrix(rnorm(9),3,3))
 ## This will generate a Mat1 function list containing the 3x3 matrix in its environment. 
 ## Mat1 is list of 4 functions: set, get, setinverse and getinverse. 
 ## Set() will change the matrix data or set it to the given argument and make the inverse NULL in the parent environment. Eg. Mat1$set(matrix(rnorm(9),3,3))
@@ -36,16 +36,21 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     
-    inv <- x$getinverse()
+	inv <- x$getinverse() 
+	
+	##Check if inverse exists
     if(!is.null(inv)) {
         message("getting cached data")
         return(inv)
     }
+	
+	##If it doesnt exist, get the matrix data and calculate inverse
     data <- x$get()
     
     ## Code for solving inverse of a matrix
     inv <- solve(data,...)
     
+	##Set the value of the inverse of the matrix
     x$setinverse(inv)
     inv
 }
